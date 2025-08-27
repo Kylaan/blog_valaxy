2f497fd2c31ef5d0159bb4420acac6e082037efe
Kylaan <815728683@qq.com>
2025-08-27 15:49:48 +0800

    鑷姩鎻愪氦

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@
 import { defineSiteConfig } from 'valaxy'
 
 export default defineSiteConfig({
-  url: 'https://kylaan.top',
+  url: 'https://kylaan.github.io/blog_valaxy/',
   lang: 'zh-CN',
   title: "Kylaan's Blog",
   subtitle: '欢迎来到我的小站',
